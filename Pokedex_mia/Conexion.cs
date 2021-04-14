5e689422aee5b54ecd620456b251b459833831b1
alberto amoros <albertamoros2204@gmail.com>
2021-04-14 14:01:56 +0200

    Agregar archivos de proyecto.

---
+++ Pokedex_mia/Conexion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;  //la libreria de MySql
+using System.Data; //la libreria del DataTable
+
+namespace PruebaBBDD
+
+{
+    class Conexion
+    {
+        public MySqlConnection conexion;//variable que se encarga de conectarnos al servidor MySql
+
+        public Conexion() // el constructor de la clase
+        {
+            conexion = new MySqlConnection("Server=127.0.0.1; Database=listaPokemons; Uid=root; Pwd=; Port=3306");
+        }
+
+        public DataTable getPokemons()
+        {
+            //como un if else pero para evitar errores
+            try {
+                conexion.Open();
+                MySqlCommand consulta = new MySqlCommand("SELECT * FROM pokemon", conexion);
+                MySqlDataReader resultado = consulta.ExecuteReader(); //Guarda el resuldado de la quarry (consulta)
+                DataTable pokemons = new DataTable(); // Formato que espera el datagridview
+                pokemons.Load(resultado);
+                conexion.Close();
+                return pokemons;
+                    
+            }
+            catch (MySqlException e){
+                throw e;
+            }
+
+        }
+        public DataTable getPokemonPorId(int _id)
+        {
+            //como un if else pero para evitar errores
+            try
+            {
+                conexion.Open();
+                MySqlCommand consulta = new MySqlCommand("SELECT * FROM pokemon WHERE id='" + _id + "'", conexion);
+                MySqlDataReader resultado = consulta.ExecuteReader(); //Guarda el resuldado de la quarry (consulta)
+                DataTable pokemons = new DataTable(); // Formato que espera el datagridview
+                pokemons.Load(resultado);
+                conexion.Close();
+                return pokemons;
+
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+
+        }
+
+    }
+
+}
---
+++ Pokedex_mia/PokedexBasica.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PruebaBBDD
+{
+    public partial class PokedexBasica : Form
+    {
+        Conexion miConexion = new Conexion();
+        int idActual = 1;
+        public PokedexBasica()
+        {
+            InitializeComponent();
+            asignaPokemon();
+        }
+
+        //boton derecha
+        private void button2_Click(object sender, EventArgs e)
+        {
+            idActual++;
+            if (idActual > 151) {
+                idActual = 1;
+            }
+            asignaPokemon();
+        }
+
+
+        //boton izquierda
+        private void button1_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            if (idActual < 1) {
+                idActual = 151;
+            }
+            asignaPokemon();
+        }
+
+        private Image convierteBlodAImagen(byte[] img)
+        {
+            MemoryStream ms = new System.IO.MemoryStream(img);
+            return (Image.FromStream(ms));
+
+        }
+
+        private void asignaPokemon() {
+            DataTable pokemonElegido = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = pokemonElegido.Rows[0]["nombre"].ToString();
+            pictureBox1.Image = convierteBlodAImagen((byte[])pokemonElegido.Rows[0]["imagen"]);
+        }
+    }
+}
